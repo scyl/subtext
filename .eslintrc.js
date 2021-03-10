352a847de95ea113b52988c3a58e94e290af07ee
Stephen <scyl@users.noreply.github.com>
2021-03-10 20:58:04 +1100

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: '.',
+      },
+    },
+  },
+  rules: {
+    'import/extensions': 'off',
+  },
+};
