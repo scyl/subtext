bbdbb63f22c038bfdc6ab74bd9622d1cc73050bb
Stephen <scyleung@gmail.com>
2021-03-13 00:47:12 +1100

    added delay to fetch retry

--- src/utils/fetch-retry.ts
+++ src/utils/fetch-retry.ts
@@ -2,26 +2,27 @@ import fetch, { RequestInit } from "node-fetch";
 
 // Fetches from specified url
 // Will retry indefinitely if the response is an error
-// TODO: add support to backoff
 export async function fetchRetry(url: string, options?: RequestInit): Promise<any> {
-  let result: any | undefined;
-
-  while (result === undefined) {
+  // eslint-disable-next-line no-constant-condition
+  while (true) {
     try {
       // eslint-disable-next-line no-await-in-loop
       const response = await fetch(url, options);
       if (response.ok) {
         // eslint-disable-next-line no-await-in-loop
-        result = await response.json();
-      } else {
-        // eslint-disable-next-line no-console
-        console.warn(`NETWORK_ERROR: unable to fetch "${url}". `, response.statusText);
+        return await response.json();
       }
+      // eslint-disable-next-line no-console
+      console.warn(`NETWORK_ERROR: unable to fetch "${url}". `, response.statusText);
     } catch (error) {
       // eslint-disable-next-line no-console
       console.warn(`NETWORK_ERROR: unable to fetch "${url}". `, error.message);
     }
+    // eslint-disable-next-line no-await-in-loop
+    await timeout(500);
   }
+}
 
-  return result;
+function timeout(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
